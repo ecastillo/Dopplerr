2dea6919db23f5df3131d641cf70b3ac2b8b296d
Eric Castillo <ecastillo11@gmail.com>
2018-11-22 22:59:35 -0600

    Initial Commit

---
+++ Dopplerr/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Dopplerr
+//
+//  Created by Eric Castillo on 11/22/18.
+//  Copyright © 2018 Eric Castillo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Dopplerr/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Dopplerr
+//
+//  Created by Eric Castillo on 11/22/18.
+//  Copyright © 2018 Eric Castillo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
